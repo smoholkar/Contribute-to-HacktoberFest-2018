26c347978209d930aad0690b64c8473aa6278e49
Eran Sevi <18116122+eransevi@users.noreply.github.com>
2018-10-21 21:29:31 +0300

    find smallest missing number
    added an efficient solution
    
    solves issue #3

--- Problem-Solving/find-smallest-missing-number.js
+++ Problem-Solving/find-smallest-missing-number.js
@@ -1,5 +1,5 @@
 /**
- * Given a sorted array of n distinct numbers where each number is in the range from 0 to m-1 and m > n. 
+ * Given a sorted array of n distinct numbers where each number is in the range from 0 to m-1 and m > n.
  * Find the smallest number that is missing from the array.
  * Examples
  *  Input: {0, 1, 2, 6, 9}, n = 5, m = 10
@@ -14,3 +14,27 @@ Output: 4
 Input: {0, 1, 2, 3, 4, 5, 6, 7, 10}, n = 9, m = 11
 Output: 8
  */
+
+function findNumber(values) {
+
+  if (values.length == 0)
+  	return -1;
+    
+  for (let i = 0; i < values.length; ++i) {
+    if (i < values[i]) {
+			return i
+    }
+  }
+
+  return values[values.length-1]+1;
+}
+
+console.log(
+	findNumber([0, 1, 2, 6, 9]),
+	findNumber([4, 5, 10, 11]),
+  findNumber([0, 1, 2, 3]),
+  findNumber([0, 1, 2, 3, 4, 5, 6, 7, 10]),
+  findNumber([])
+);
+
+//3 0 4 8 -1
