b6f6809536204d61f3d4dacf55c456893ae24c72
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-21 14:36:42 -0400

    Adds a brief explanation to the solution

--- Problem-Solving/find-smallest-missing-number.js
+++ Problem-Solving/find-smallest-missing-number.js
@@ -20,8 +20,21 @@ Solution written by:
 	@andresitorresm on twitter
  */
 
+/**
+ * Given a sorted array of n numbers in the range [0, m), 
+ * returns the smallest number that is missing from the array
+ * 
+ * @param  {array of integer} number_list Sorted array of distinct numbers
+ * @param  {int} n           Amount of elements in the number list
+ * @param  {int} m           Number denoting the first element that is not in the list
+ * @return {int}             The smallest missing number
+ */
 function smallest_missing_number(number_list, n, m) {
+	// We iterate on the numbers from 0 to m-1
 	for (var i = 0; i < m; i++) {
+		// If one number does not correspond to its
+		// marked position on the number list,
+		// return it as the first missing number
 		if (i != number_list[i]) {
 			return i;
 		}
