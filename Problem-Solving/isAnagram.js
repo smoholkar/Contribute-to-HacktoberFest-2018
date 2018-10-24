7f92ddf6cb7bea6f72023eb94f32e37f939d813b
Wagner Souza <wagh.cwb@gmail.com>
2018-10-24 10:05:38 -0300

    Adds option to use all words or not

--- Problem-Solving/isAnagram.js
+++ Problem-Solving/isAnagram.js
@@ -1,12 +1,38 @@
-function isAnagram(firstWord, secondWord) {
-  const word1 = firstWord.replace(/\s/, '').split('')
-  const word2 = secondWord.replace(/\s/, '').split('')
+function isAnagram(word1, word2, useAll) {
+  //default need use all
+  //Anagrams don't necessarily have to use all the letters, but they normally do.
+  if (typeof useAll === 'undefined' || typeof useAll !== 'boolean') {
+    useAll = true;
+  }
   
-  word1.sort()
-  word2.sort()
+  //check string only
+  if (typeof word1 !== 'string' && typeof word1 !== 'string') {
+    throw Error('Invalid argument! Only String type is allow to compare!');
+  }
   
-  return JSON.stringify(word1) === JSON.stringify(word2)
+  //normalise to array
+  let arr1 = word1.replace(/\s/g, "").toLowerCase().split('').sort();
+  let arr2 = word2.replace(/\s/g, "").toLowerCase().split('').sort();
+  
+  //check anagrams that must use exatcly all
+  if (useAll || arr1.length === arr2.length) {
+    return arr1.join('') === arr2.join('');
+  }
+  
+  //determine which is part of another
+  let whole = arr1.length > arr2.length ? arr1 : arr2;
+  let part = arr2.length < arr1.length ? arr2 : arr1;
+  
+  for (let i = 0; i < whole.length; i++) {
+    let index = part.indexOf(whole[i]);
+    part.splice(index, 1);
+  }
+  
+  return part.length === 0;
 }
 
-console.log(isAnagram('abc', 'cba'))
-
+//example use
+isAnagram('why','how') // expected output: false
+isAnagram('funeral','real fun') // expected output: true
+isAnagram('funeral','real funs') // expected output: false
+isAnagram('funeral','real funs', false) // expected output: true
